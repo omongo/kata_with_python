afd22da54627016ecc69539382b09cc51398c940
Roof Panichsombat <roofimon@gmail.com>
2014-02-21 19:27:06 +0700

    Game P1

--- tennis.py
+++ tennis.py
@@ -3,17 +3,21 @@ class TennisScoreBoard:
         self.p1_score = 0
         self.p2_score = 0
 
-    def getScore(self):
-        p1_wording = "Love"
-        p2_wording = "Love"
-        if self.p1_score == 1: 
-            p1_wording = "Fifteen"
-            if self.p2_score == 1: 
-                p2_wording = "All"
-            if self.p2_score == 2:
-                p2_wording = "Thirty"
-        return "%s %s" % ( p1_wording, p2_wording)
+    def get_score(self):
+        score = {0: 'Love', 1: 'Fifteen', 2: 'Thirty', 3: 'Forty'}
+        if self.p1_score == self.p2_score:
+            if self.p1_score == 3:
+                return "Deuce"
+            elif self.p1_score != 0:
+                return "{} All".format(score[self.p1_score])
+        if (self.p1_score - self.p2_score ==1) and (self.p1_score + self.p2_score > 6):
+            return "Advantage P1"
+        if (self.p1_score - self.p2_score ==2) and (self.p1_score + self.p2_score > 7):
+            return "Game P1"
+        return "{} {}".format(score[self.p1_score], score[self.p2_score])
 
-    def wonScore(self, name):
-        if name=="p1": self.p1_score += 1
-        if name=="p2": self.p2_score += 1
+    def won_p1_score(self):
+        self.p1_score += 1
+
+    def won_p2_score(self):
+        self.p2_score += 1
--- test_tennis.py
+++ test_tennis.py
@@ -1,31 +1,69 @@
 import unittest
 from tennis import TennisScoreBoard
+
+
 class TestTennis(unittest.TestCase):
     def setUp(self):
         self.tennisScoreBoard = TennisScoreBoard()
-    
+
     def testLoveLove(self):
         expected = "Love Love"
-        result = self.tennisScoreBoard.getScore()
+        result = self.tennisScoreBoard.get_score()
         self.assertEquals(expected, result)
 
     def testFifteenLove(self):
         expected = "Fifteen Love"
-        self.tennisScoreBoard.wonScore('p1')
-        result = self.tennisScoreBoard.getScore()
+        self.tennisScoreBoard.won_p1_score()
+        result = self.tennisScoreBoard.get_score()
         self.assertEquals(expected, result)
 
     def testFifteenAll(self):
         expected = "Fifteen All"
-        self.tennisScoreBoard.wonScore('p1')
-        self.tennisScoreBoard.wonScore('p2')
-        result = self.tennisScoreBoard.getScore()
+        self.tennisScoreBoard.won_p1_score()
+        self.tennisScoreBoard.won_p2_score()
+        result = self.tennisScoreBoard.get_score()
         self.assertEquals(expected, result)
-    
+
     def testFifteenThirty(self):
         expected = "Fifteen Thirty"
-        self.tennisScoreBoard.wonScore('p1')
-        self.tennisScoreBoard.wonScore('p2')
-        self.tennisScoreBoard.wonScore('p2')
-        result = self.tennisScoreBoard.getScore()
+        self.tennisScoreBoard.won_p1_score()
+        for i in xrange(2):
+            self.tennisScoreBoard.won_p2_score()
+        result = self.tennisScoreBoard.get_score()
+        self.assertEquals(expected, result)
+
+    def testThirtyAll(self):
+        expected = "Thirty All"
+        for i in xrange(2):
+            self.tennisScoreBoard.won_p1_score()
+        for i in xrange(2):
+            self.tennisScoreBoard.won_p2_score()
+        result = self.tennisScoreBoard.get_score()
+        self.assertEquals(expected, result)
+
+    def testDeuce(self):
+        expected = "Deuce"
+        for i in xrange(3):
+            self.tennisScoreBoard.won_p1_score()
+        for i in xrange(3):
+            self.tennisScoreBoard.won_p2_score()
+        result = self.tennisScoreBoard.get_score()
+        self.assertEquals(expected, result)
+
+    def testAdvantageP1(self):
+        expected = "Advantage P1"
+        for i in xrange(4):
+            self.tennisScoreBoard.won_p1_score()
+        for i in xrange(3):
+            self.tennisScoreBoard.won_p2_score()
+        result = self.tennisScoreBoard.get_score()
+        self.assertEquals(expected, result)
+    
+    def testGame(self):
+        expected = "Game P1"
+        for i in xrange(5):
+            self.tennisScoreBoard.won_p1_score()
+        for i in xrange(3):
+            self.tennisScoreBoard.won_p2_score()
+        result = self.tennisScoreBoard.get_score()
         self.assertEquals(expected, result)
