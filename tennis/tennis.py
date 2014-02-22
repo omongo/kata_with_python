bbb375824af74c3a6ba4a17a482c5a4a5c862dd0
Chanathip Srithanrat <chanathip@proteus-tech.com>
2014-02-23 03:36:03 +0700

    Refactor the codes

--- tennis/tennis.py
+++ tennis/tennis.py
@@ -4,17 +4,17 @@ class TennisScoreBoard:
         self.p2_score = 0
 
     def get_score(self):
-        points = ('Love', 'Fifteen', 'Thirty', 'Forty')
+        pts = ('Love', 'Fifteen', 'Thirty', 'Forty')
         p1 = self.p1_score
         p2 = self.p2_score
         if (p1 + p2) >= 6:
-            if p1 == p2:
-                return 'Deuce'
-            player = 'P1' if p1 > p2 else 'P2'
-            return ('Game ' if abs(p1 - p2) == 2 else 'Advantage ') + player
-        return points[p1] + ' - ' + (points[p2] if p1 != p2 or p1 == 0 else 'All')
+            if p1 == p2: return 'Deuce'
+            p = 'P1' if p1 > p2 else 'P2'
+            return ('Game ' if abs(p1 - p2) == 2 else 'Advantage ') + p
+        return pts[p1] + ' - ' + (pts[p2] if p1 != p2 or p1 == 0 else 'All')
 
     def won_p1(self):
+        self.points = []
         self.p1_score += 1
 
     def won_p2(self):
